9ea06c29195ff707f12040c059140bcd878b02af
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-12-07 19:41:52 +0700

    Deploy

---
+++ src/components/Mem.vue
@@ -0,0 +1,99 @@
+<template>
+  <div >
+    <div class="columns is-multiline is-gapless is-mobile ">
+      <div class="column is-one-quarter"><img src="../assets/IMG_6104.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5203.jpg"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5204.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5205.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5206.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5208.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5209.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5210.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5219.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5220.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5221.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5222.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5231.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5232.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5240.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5241.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5242.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5244.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5245.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5249.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5250.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5251.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5252.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5253.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5254.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5255.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5256.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5262.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5263.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5267.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5268.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5270.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5271.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5277.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5314.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5320.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5321.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5329.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5330.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5331.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5332.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5333.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5334.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5335.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5394.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5395.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5414.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5430.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5456.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5500.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5502.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5503.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5504.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5652.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5656.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5657.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5659.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5821.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5822.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5825.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5828.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5830.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5842.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5843.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5846.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5848.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5950.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5963.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_5994.JPG"/></div>
+      <div class="column is-one-quarter"><img src="../assets/IMG_6013.JPG"/></div>
+    </div>
+    <!-- <div v-for="item in list">
+    <img :src="item" v-bind:alt="pic"/>
+  </div> -->
+</div>
+</template>
+
+<script>
+let list = ['../assets/IMG_6104.JPG', '../assets/IMG_5203.JPG', '../assets/IMG_5204.JPG', '../assets/IMG_5205.JPG', '../assets/IMG_5206.JPG', '../assets/IMG_5208.JPG', '../assets/IMG_5209.JPG', '../assets/IMG_5210.JPG', '../assets/IMG_5219.JPG', '../assets/IMG_5220.JPG', '../assets/IMG_5221.JPG', '../assets/IMG_5222.JPG', '../assets/IMG_5231.JPG', '../assets/IMG_5232.JPG', '../assets/IMG_5240.JPG', '../assets/IMG_5241.JPG', '../assets/IMG_5242.JPG', '../assets/IMG_5244.JPG', '../assets/IMG_5245.JPG', '../assets/IMG_5249.JPG', '../assets/IMG_5250.JPG', '../assets/IMG_5251.JPG', '../assets/IMG_5252.JPG', '../assets/IMG_5253.JPG', '../assets/IMG_5254.JPG', '../assets/IMG_5255.JPG', '../assets/IMG_5256.JPG', '../assets/IMG_5262.JPG', '../assets/IMG_5263.JPG', '../assets/IMG_5267.JPG', '../assets/IMG_5268.JPG', '../assets/IMG_5270.JPG', '../assets/IMG_5271.JPG', '../assets/IMG_5277.JPG', '../assets/IMG_5314.JPG', '../assets/IMG_5320.JPG', '../assets/IMG_5321.JPG', '../assets/IMG_5329.JPG', '../assets/IMG_5330.JPG', '../assets/IMG_5331.JPG', '../assets/IMG_5332.JPG', '../assets/IMG_5333.JPG', '../assets/IMG_5334.JPG', '../assets/IMG_5335.JPG', '../assets/IMG_5394.JPG', '../assets/IMG_5395.JPG', '../assets/IMG_5414.JPG', '../assets/IMG_5430.JPG', '../assets/IMG_5456.JPG', '../assets/IMG_5500.JPG', '../assets/IMG_5502.JPG', '../assets/IMG_5503.JPG', '../assets/IMG_5504.JPG', '../assets/IMG_5652.JPG', '../assets/IMG_5656.JPG', '../assets/IMG_5657.JPG', '../assets/IMG_5659.JPG', '../assets/IMG_5821.JPG', '../assets/IMG_5822.JPG', '../assets/IMG_5825.JPG', '../assets/IMG_5828.JPG', '../assets/IMG_5830.JPG', '../assets/IMG_5842.JPG', '../assets/IMG_5843.JPG', '../assets/IMG_5846.JPG', '../assets/IMG_5848.JPG', '../assets/IMG_5950.JPG', '../assets/IMG_5963.JPG', '../assets/IMG_5994.JPG', '../assets/IMG_6013.JPG']
+export default {
+  data () {
+    return {
+      list : list
+    }
+  },
+  computed: {},
+  mounted () {},
+  methods: {},
+  components: {
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Topping.vue
@@ -0,0 +1,40 @@
+<template lang="html">
+  <div class="topping section " align="center">
+    <img class="circle image is-128x128" src="../assets/head.jpg"/>
+    <h1 class="title is-1">{{msg}}</h1>
+    <h1 class="title is-2">ขอให้มีความสุขมากๆ สุขภาพแข็งแรงนะครับ</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg:"Happy Birthday My Father <3"
+    }
+  },
+  computed: {},
+  mounted () {},
+  methods: {},
+  components: {}
+}
+</script>
+<style>
+.topping{
+  top: 50%;
+  left:50%;
+  position: fixed;
+  z-index: 999;
+	transform: translateY(-50%) translateX(-50%);
+}
+.section {
+  /*border-radius: 2%;*/
+  background: #ffffff;
+  opacity: 0.9;
+  width: 100%;
+  font-family: 'Prompt', sans-serif;
+}
+.circle{
+  border-radius: 100%;
+}
+</style>
